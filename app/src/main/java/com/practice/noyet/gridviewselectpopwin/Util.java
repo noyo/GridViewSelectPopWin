3764cef8c20073a406a87e0c075800bb2474c362
noyet <1471551330@qq.com>
2015-11-01 15:14:53 +0800

    Initial commit

---
+++ app/src/main/java/com/practice/noyet/gridviewselectpopwin/Util.java
@@ -0,0 +1,22 @@
+package com.practice.noyet.gridviewselectpopwin;
+
+import android.content.Context;
+import android.util.DisplayMetrics;
+import android.view.WindowManager;
+
+/**
+ * Created by noyet on 2015/11/1.
+ */
+public class Util {
+    /**
+     * 获取当前屏幕的尺寸大小
+     * @param context
+     * @return
+     */
+    public static DisplayMetrics getMetrics(Context context) {
+        DisplayMetrics metrics = new DisplayMetrics();
+        WindowManager manager = (WindowManager) context.getSystemService(Context.WINDOW_SERVICE);
+        manager.getDefaultDisplay().getMetrics(metrics);
+        return metrics;
+    }
+}
